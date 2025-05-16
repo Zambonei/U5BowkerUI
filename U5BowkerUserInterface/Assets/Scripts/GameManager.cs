88e9aab0fb432538dc39970327b97da90146258c
Zambonei <726926@student.dusd.net>
2025-05-16 11:43:20 -0700

    a

--- U5BowkerUserInterface/Assets/Scripts/GameManager.cs
+++ U5BowkerUserInterface/Assets/Scripts/GameManager.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreTest;
+    private int score;
+    public TextMeshProUGUI gameOverText;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+         
     }
 
     // Update is called once per frame
@@ -19,8 +26,6 @@ public class GameManager : MonoBehaviour
 
     }
 
-   
-
     IEnumerator SpawnTarget()
     {
         while (true)
@@ -28,8 +33,20 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+
         }
 
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreTest.text = "Score: " + score;
+    }
+        
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
     }
 }
    
--- U5BowkerUserInterface/Assets/Scripts/Target.cs
+++ U5BowkerUserInterface/Assets/Scripts/Target.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRB;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
 
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
     // Start is called before the first frame update
     void Start()
     {
         targetRB = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         
         targetRB.AddForce(RandomForce(), ForceMode.Impulse);
         targetRB.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
@@ -24,11 +29,14 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        gameManager.GameOver();
     }
     // Update is called once per frame
     void Update()
